cd0a1b12a85bc2bb9af02734631160025e02c26f
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-08-03 22:42:30 -0500

    fixed the bug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -11,7 +11,7 @@ import {
 import Auth from '../utils/auth';
 import { useMutation } from "@apollo/client";
 import { SAVE_BOOK } from "../utils/mutations";
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -75,12 +75,12 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook({
-        variables: {bookData: {...bookToSave}}
+        variables: {bookData: {...bookToSave}},
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
