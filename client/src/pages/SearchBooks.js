8dee89149db4df00c93ff9a380b0de03766f831e
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-08-03 22:45:27 -0500

    cleaned up a couple things

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -77,11 +77,6 @@ const SearchBooks = () => {
       const response = await saveBook({
         variables: {bookData: {...bookToSave}},
       });
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
